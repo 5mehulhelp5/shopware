e429078991856f385f1c234a3aa3d68ea868e1c9
Sy Dinh <s.dinh@shopware.com>
2025-04-17 14:50:51 +0700

    feat: add product measurement card

---
+++ src/Administration/Resources/app/administration/src/module/sw-product/component/sw-product-measurement-form/index.js
@@ -0,0 +1,36 @@
+import template from './sw-product-measurement-form.html.twig';
+import './sw-product-measurement-form.scss';
+
+const { Mixin } = Shopware;
+const { mapPropertyErrors } = Shopware.Component.getComponentHelper();
+
+/*
+ * @sw-package inventory
+ * @private
+ */
+export default {
+    template,
+
+    mixins: [
+        Mixin.getByName('placeholder'),
+    ],
+
+    props: {
+        allowEdit: {
+            type: Boolean,
+            required: true,
+        },
+    },
+
+    computed: {
+        product() {
+            return Shopware.Store.get('swProductDetail').product;
+        },
+
+        parentProduct() {
+            return Shopware.Store.get('swProductDetail').parentProduct;
+        },
+
+        ...mapPropertyErrors('product', ['width', 'height', 'length', 'weight']),
+    },
+};
