90fcbc5128853da91690915e50fb0486324e6d7a
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-06-04 11:01:22 +0200

    added logger to middleware

--- src/Core/Framework/MessageQueue/Stats/StatsService.php
+++ src/Core/Framework/MessageQueue/Stats/StatsService.php
@@ -35,19 +35,19 @@ class StatsService
 
     public function registerMessage(Envelope $envelope): void
     {
-        $this->logger->warning('Starting to register message', [
+        $this->logger->error('Starting to register message', [
             'message_class' => \get_class($envelope->getMessage()),
         ]);
 
         if (!$this->enabled) {
-            $this->logger->warning('Message registration skipped - stats service is disabled');
+            $this->logger->error('Message registration skipped - stats service is disabled');
 
             return;
         }
 
         $sentAtStamp = $envelope->last(SentAtStamp::class);
         if ($sentAtStamp === null) {
-            $this->logger->warning('Message registration skipped - missing SentAtStamp');
+            $this->logger->error('Message registration skipped - missing SentAtStamp');
 
             return;
         }
@@ -56,7 +56,7 @@ class StatsService
         $messageFqcn = \get_class($envelope->getMessage());
         $this->statsRepository->updateMessageStats($messageFqcn, $timeInQueue);
 
-        $this->logger->warning('Message registration completed successfully', [
+        $this->logger->error('Message registration completed successfully', [
             'message_class' => $messageFqcn,
             'time_in_queue' => $timeInQueue,
         ]);
