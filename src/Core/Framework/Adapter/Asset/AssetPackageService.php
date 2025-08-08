12cbcff26affa21f7d62962947190b470cfd4a97
Michael Telgmann <m.telgmann@shopware.com>
2025-08-08 15:10:46 +0200

    WIP

--- src/Core/Framework/Adapter/Asset/AssetPackageService.php
+++ src/Core/Framework/Adapter/Asset/AssetPackageService.php
@@ -2,6 +2,7 @@
 
 namespace Shopware\Core\Framework\Adapter\Asset;
 
+use League\Flysystem\FilesystemOperator;
 use Shopware\Core\DevOps\Environment\EnvironmentHelper;
 use Shopware\Core\Framework\Adapter\AdapterException;
 use Shopware\Core\Framework\Log\Package;
@@ -11,6 +12,7 @@ use Symfony\Component\Asset\Package as AssetPackage;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Asset\UrlPackage;
 use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+use Symfony\Component\Filesystem\Path;
 
 #[Package('framework')]
 class AssetPackageService
@@ -18,19 +20,26 @@ class AssetPackageService
     /**
      * @param array<string, string> $bundleMap
      */
-    public static function create(array $bundleMap, AssetPackage $package, VersionStrategyInterface $versionStrategy, mixed ...$args): Packages
-    {
+    public static function create(
+        array $bundleMap,
+        AssetPackage $package,
+        VersionStrategyInterface $versionStrategy,
+        FilesystemOperator $assetFilesystem,
+        mixed ...$args
+    ): Packages {
         $packages = new Packages(...$args);
 
         if (!EnvironmentHelper::hasVariable('APP_URL')) {
             return $packages;
         }
 
+        $currentBundlesPath = trim($assetFilesystem->read('/bundles/path'));
         foreach ($bundleMap as $bundleName => $bundlePath) {
             /** @see AssetService::getTargetDirectory() */
-            $targetPath = '/bundles/' . preg_replace('/bundle$/', '', mb_strtolower($bundleName));
+            $targetPath = Path::join('/bundles', $currentBundlesPath, preg_replace('/bundle$/', '', mb_strtolower($bundleName)));
 
             $path = $package->getUrl($targetPath);
+            dump($targetPath, $path);
 
             try {
                 $bundlePackage = new UrlPackage($path, new PrefixVersionStrategy($targetPath, $versionStrategy));
@@ -40,6 +49,8 @@ class AssetPackageService
 
             $packages->addPackage('@' . $bundleName, $bundlePackage);
         }
+
+//        dd($packages);
 
         return $packages;
     }
--- src/Core/Framework/DependencyInjection/CompilerPass/AssetBundleRegistrationCompilerPass.php
+++ src/Core/Framework/DependencyInjection/CompilerPass/AssetBundleRegistrationCompilerPass.php
@@ -32,6 +32,7 @@ class AssetBundleRegistrationCompilerPass implements CompilerPassInterface
         }
 
         $arguments = $assetService->getArguments();
+        array_unshift($arguments, new Reference('shopware.filesystem.asset'));
         array_unshift($arguments, new Reference('shopware.asset.public.version_strategy'));
         array_unshift($arguments, new Reference('shopware.asset.asset_without_versioning'));
         array_unshift($arguments, $bundleMap);
